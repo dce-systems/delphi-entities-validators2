925eb9f022e72ea09f8617c3b105fb5a15582d1e
dce-systems <31881063+dce-systems@users.noreply.github.com>
2017-09-13 15:05:12 +0200

    Add files via upload
    * Highlight color can be modify from runtime.
    * Form component can show hint with invalid rules list.

--- source/Validators.Engine.pas
+++ source/Validators.Engine.pas
@@ -2,9 +2,11 @@
 //Modified by DCE-Systems [https://github.com/dce-systems]
 //
 //Changes:
-//        1) BrokenRules can be returned as String
-//        2) Form component can be bind to property
-//        3) Form component can be highlight when binded property is invalid
+//        1) BrokenRules can be returned as String.
+//        2) Form component can be bind to property.
+//        3) Form component can be highlight when binded property is invalid.
+//        4) Highlight color can be modify from runtime.
+//        5) Form component can show hint with invalid rules list.
 
 unit Validators.Engine;
 
@@ -12,50 +14,69 @@ interface
 
 uses
   System.RTTI, System.JSON, System.Generics.Collections, System.Classes, Vcl.StdCtrls,
-  Vcl.Graphics;
+  Vcl.Graphics, Vcl.Controls;
 
 type
-
   TBrokenRules = array of string;
-  TBrokenFields = TDictionary<String, TObject>;
+
+  TBrokenFields = TDictionary<string, TBrokenRules>;
+
+  TBrokenObjects = TDictionary<string, TObject>;
 
   TBrokenRulesHelper = record helper for TBrokenRules
-  private
+  public
     function asJsonArray: TJSONArray;
   end;
 
   IValidationResult = interface
     ['{14B7A1EF-DCD0-416C-BAF1-CF25A6F6E202}']
     function GetBrokenRules: TBrokenRules;
-    function GetBrokenRulesText: String;
+    function GetBrokenRulesText: string;
+    function GetBrokenFields: TBrokenFields;
+    function GetBrokenObjects: TBrokenObjects;
+    function GetValidationColor: TColor;
     procedure SetBrokenRules(aBrokenRules: TBrokenRules);
+    procedure SetValidationColor(AValidationColor: TColor);
     procedure AddBrokenRules(aBrokenRules: TBrokenRules);
-    procedure AddBrokenField(aBrokenField: String);
+    procedure AddBrokenField(aBrokenField: string; aBrokenRules: TBrokenRules);
     property BrokenRules: TBrokenRules read GetBrokenRules write SetBrokenRules;
-    property BrokenRulesText: String read GetBrokenRulesText;
+    property BrokenRulesText: string read GetBrokenRulesText;
+    property BrokenFields: TBrokenFields read GetBrokenFields;
+    property BrokenObjects: TBrokenObjects read GetBrokenObjects;
+    property ValidationColor: TColor read GetValidationColor write SetValidationColor;
     function IsValid(const aRaiseExceptionIfNot: boolean = False; AHighlightInvalidComponents: Boolean = True): boolean;
-    procedure Bind(const AProperty: String; const AComponentName: TObject);
+    procedure Bind(const AProperty: string; const AComponentName: TObject);
     procedure HighlightInvalidComponents(AClearText: Boolean = False);
   end;
 
   TValidationResult = class(TInterfacedObject, IValidationResult)
   private
+    FValidationColor: TColor;
     procedure SetHighlight(const AObject: TObject; AColor: TColor; AClearText: Boolean);
+    procedure SetHint(const AObject: TObject; ABrokenRules: TBrokenRules);
   protected
     FBrokenRules: TBrokenRules;
     FBrokenFields: TBrokenFields;
+    FBrokenObjects: TBrokenObjects;
   public
     constructor Create;
     destructor Destroy; override;
     function GetBrokenRules: TBrokenRules;
-    function GetBrokenRulesText: String;
+    function GetBrokenRulesText: string;
+    function GetBrokenFields: TBrokenFields;
+    function GetBrokenObjects: TBrokenObjects;
+    function GetValidationColor: TColor;
     procedure SetBrokenRules(aBrokenRules: TBrokenRules);
+    procedure SetValidationColor(AValidationColor: TColor);
     procedure AddBrokenRules(aBrokenRules: TBrokenRules);
-    procedure AddBrokenField(aBrokenField: String);
+    procedure AddBrokenField(aBrokenField: string; aBrokenRules: TBrokenRules);
     property BrokenRules: TBrokenRules read GetBrokenRules write SetBrokenRules;
-    property BrokenRulesText: String read GetBrokenRulesText;
+    property BrokenRulesText: string read GetBrokenRulesText;
+    property BrokenFields: TBrokenFields read GetBrokenFields;
+    property BrokenObjects: TBrokenObjects read GetBrokenObjects;
+    property ValidationColor: TColor read GetValidationColor write SetValidationColor;
     function IsValid(const aRaiseExceptionIfNot: boolean = False; AHighlightInvalidComponents: Boolean = True): boolean;
-    procedure Bind(const AProperty: String; const AComponentName: TObject);
+    procedure Bind(const AProperty: string; const AComponentName: TObject);
     procedure HighlightInvalidComponents(AClearText: Boolean = False);
   end;
 
@@ -66,15 +87,13 @@ type
 
   IValidatable<T> = interface
     ['{01643E54-2058-4C42-BD98-462EA78E1CAB}']
-    function Validate(aValidator: IValidator<T>;
-      out aBrokenRules: TBrokenRules): boolean;
+    function Validate(aValidator: IValidator<T>; out aBrokenRules: TBrokenRules): boolean;
   end;
 
   IValidatorContainer = interface
     ['{DCC0B831-822B-4159-B132-10587E8BFDFB}']
     procedure RegisterValidatorFor(aType: TClass; aContext: string);
-    function GetValidatorFor(aType: TClass; aContext: string)
-      : IValidator<TClass>;
+    function GetValidatorFor(aType: TClass; aContext: string): IValidator<TClass>;
   end;
 
   TBaseValidatorContainer = class(TObject)
@@ -83,26 +102,23 @@ type
   public
     constructor Create;
     destructor Destroy; override;
-    procedure RegisterValidatorFor<T: class>(aContext: string;
-      aValidator: IValidator<T>);
+    procedure RegisterValidatorFor<T: class>(aContext: string; aValidator: IValidator<T>);
     function GetValidatorFor<T: class>(aContext: string): IValidator<T>;
   end;
 
   TValidationEngine = class(TObject)
   private
-    class var FRTTIContext: TRttiContext;
-    class var FValidationContainer: TBaseValidatorContainer;
+    class var
+      FRTTIContext: TRttiContext;
+    class var
+      FValidationContainer: TBaseValidatorContainer;
   public
     class constructor Create;
     class destructor Destroy;
-    class function Validate<T: class>(aObject: T; aContext: string)
-      : IValidationResult;
-    class function PropertyValidation(aObject: TObject; aContext: string)
-      : IValidationResult;
-    class function EntityValidation<T: class>(aObject: T; aContext: string)
-      : IValidationResult;
-    class property ValidationContainer: TBaseValidatorContainer
-      read FValidationContainer;
+    class function Validate<T: class>(aObject: T; aContext: string): IValidationResult;
+    class function PropertyValidation(aObject: TObject; aContext: string): IValidationResult;
+    class function EntityValidation<T: class>(aObject: T; aContext: string): IValidationResult;
+    class property ValidationContainer: TBaseValidatorContainer read FValidationContainer;
   end;
 
 implementation
@@ -135,8 +151,7 @@ begin
   FValidationContainer.Free;
 end;
 
-class function TValidationEngine.EntityValidation<T>(aObject: T;
-  aContext: string): IValidationResult;
+class function TValidationEngine.EntityValidation<T>(aObject: T; aContext: string): IValidationResult;
 var
   a: TCustomAttribute;
   lValidator: IValidator<T>;
@@ -146,8 +161,7 @@ begin
   Result.AddBrokenRules(lValidator.Validate(aObject).BrokenRules);
 end;
 
-class function TValidationEngine.PropertyValidation(aObject: TObject;
-  aContext: string): IValidationResult;
+class function TValidationEngine.PropertyValidation(aObject: TObject; aContext: string): IValidationResult;
 var
   rt: TRttiType;
   a: TCustomAttribute;
@@ -159,7 +173,7 @@ begin
   for p in rt.GetProperties do
     for a in p.GetAttributes do
     begin
-      if not(a is ValidationAttribute) then
+      if not (a is ValidationAttribute) then
         continue;
       if ValidationAttribute(a).Context <> aContext then
         continue;
@@ -167,16 +181,14 @@ begin
       if m = nil then
         continue;
 
-      if m.Invoke(a, [p.GetValue(aObject).AsString]).AsType < IValidationResult > .BrokenRules.asJsonArray.ToString <> '[]' then
-        Result.AddBrokenField(p.Name);
+      if Length(m.Invoke(a, [p.GetValue(aObject).AsString]).AsType < IValidationResult > .BrokenRules) > 0 then
+        Result.AddBrokenField(p.Name, m.Invoke(a, [p.GetValue(aObject).AsString]).AsType < IValidationResult > .BrokenRules);
 
-      Result.AddBrokenRules(m.Invoke(a, [p.GetValue(aObject).AsString])
-        .AsType<IValidationResult>.BrokenRules);
+      Result.AddBrokenRules(m.Invoke(a, [p.GetValue(aObject).AsString]).AsType < IValidationResult > .BrokenRules);
     end;
 end;
 
-class function TValidationEngine.Validate<T>(aObject: T; aContext: string)
-  : IValidationResult;
+class function TValidationEngine.Validate<T>(aObject: T; aContext: string): IValidationResult;
 var
   rt: TRttiType;
   cx: TRttiContext;
@@ -194,7 +206,7 @@ end;
 
 constructor TBaseValidatorContainer.Create;
 begin
-  FRegistry := TDictionary < TClass, TDictionary < string, IInterface >>.Create;
+  FRegistry := TDictionary<TClass, TDictionary<string, IInterface>>.Create;
 end;
 
 destructor TBaseValidatorContainer.Destroy;
@@ -207,14 +219,12 @@ begin
   inherited;
 end;
 
-function TBaseValidatorContainer.GetValidatorFor<T>(aContext: string)
-  : IValidator<T>;
+function TBaseValidatorContainer.GetValidatorFor<T>(aContext: string): IValidator<T>;
 begin
   Result := FRegistry[T][aContext] as IValidator<T>;
 end;
 
-procedure TBaseValidatorContainer.RegisterValidatorFor<T>(aContext: string;
-  aValidator: IValidator<T>);
+procedure TBaseValidatorContainer.RegisterValidatorFor<T>(aContext: string; aValidator: IValidator<T>);
 var
   lDictionary: TDictionary<string, IInterface>;
 begin
@@ -231,12 +241,27 @@ begin
   FBrokenRules := FBrokenRules + aBrokenRules;
 end;
 
-procedure TValidationResult.AddBrokenField(aBrokenField: String);
+procedure TValidationResult.AddBrokenField(aBrokenField: string; aBrokenRules: TBrokenRules);
+var
+  Temp: TBrokenRules;
 begin
   if FBrokenFields.ContainsKey(aBrokenField) then
-    FBrokenFields.Remove(aBrokenField);
+  begin
+    Temp := FBrokenFields.Items[aBrokenField] + aBrokenRules;
+    FBrokenFields.AddOrSetValue(aBrokenField, Temp);
+  end
+  else
+    FBrokenFields.Add(aBrokenField, aBrokenRules);
+end;
 
-  FBrokenFields.Add(aBrokenField, nil);
+function TValidationResult.GetBrokenFields: TBrokenFields;
+begin
+  Result := FBrokenFields;
+end;
+
+function TValidationResult.GetBrokenObjects: TBrokenObjects;
+begin
+  Result := FBrokenObjects;
 end;
 
 function TValidationResult.GetBrokenRules: TBrokenRules;
@@ -244,7 +269,7 @@ begin
   Result := FBrokenRules;
 end;
 
-function TValidationResult.GetBrokenRulesText: String;
+function TValidationResult.GetBrokenRulesText: string;
 var
   Rule: string;
 begin
@@ -252,6 +277,11 @@ begin
   begin
     Result := Trim(Result + #13#10 + Rule);
   end;
+end;
+
+function TValidationResult.GetValidationColor: TColor;
+begin
+  Result := FValidationColor;
 end;
 
 function TValidationResult.IsValid(const aRaiseExceptionIfNot: boolean = False; AHighlightInvalidComponents: Boolean = True): boolean;
@@ -267,34 +297,43 @@ begin
   end;
 end;
 
-procedure TValidationResult.Bind(const AProperty: String; const AComponentName: TObject);
+procedure TValidationResult.Bind(const AProperty: string; const AComponentName: TObject);
 begin
-  if FBrokenFields.ContainsKey(AProperty) then
-    FBrokenFields.AddOrSetValue(AProperty, AComponentName);
+  if FBrokenObjects.ContainsKey(AProperty) then
+    FBrokenObjects.Remove(AProperty);
+
+  FBrokenObjects.Add(AProperty, AComponentName);
   SetHighlight(AComponentName, clWindow, False);
 end;
 
 procedure TValidationResult.HighlightInvalidComponents(AClearText: Boolean = False);
-const
-  COLOR = $8080F0; //Light Coral 	240-128-128
 var
-  Field: TPair<string, TObject>;
+  BrokenField: TPair<string, TBrokenRules>;
+  BrokenObject: TPair<string, TObject>;
 begin
-  for Field in FBrokenFields do
-    if not Assigned(Field.Value) then
-      Continue
-    else
-      SetHighlight(Field.Value, COLOR, AClearText);
+  for BrokenField in FBrokenFields do
+    if Length(BrokenField.Value) > 0 then
+      for BrokenObject in FBrokenObjects do
+        if BrokenField.Key = BrokenObject.Key then
+          if Assigned(BrokenObject.Value) then
+          begin
+            SetHighlight(BrokenObject.Value, FValidationColor, AClearText);
+            SetHint(BrokenObject.Value, BrokenField.Value);
+          end;
 end;
 
 constructor TValidationResult.Create;
 begin
-  FBrokenFields := TDictionary<String, TObject>.Create();
+  FBrokenFields := TDictionary<string, TBrokenRules>.Create();
+  FBrokenObjects := TDictionary<string, TObject>.Create();
+
+  FValidationColor := $8080F0; //Light Coral 	240-128-128
 end;
 
 destructor TValidationResult.Destroy;
 begin
   FBrokenFields.Free;
+  FBrokenObjects.Free;
   inherited;
 end;
 
@@ -304,35 +343,46 @@ begin
 end;
 
 procedure TValidationResult.SetHighlight(const AObject: TObject; AColor: TColor; AClearText: Boolean);
+var
+  RTTIContext: TRttiContext;
+  RttiType: TRttiType;
+  RttiProperty: TRttiProperty;
 begin
   if Assigned(AObject) then
   begin
-    if AObject is TCustomEdit then
+    RttiType := RTTIContext.GetType(AObject.ClassType);
+    if Assigned(RttiType) then
     begin
-      if AObject is TEdit then
-      begin
-        if AClearText then
-          (AObject as TEdit).Clear;
-        (AObject as TEdit).Color := AColor;
-      end
-      else if AObject is TMemo then
-      begin
-        if AClearText then
-          (AObject as TMemo).Clear;
-        (AObject as TMemo).Color := AColor;
-      end;
-    end
-    else if AObject is TCheckBox then
-      (AObject as TCheckBox).Color := AColor
-    else if AObject is TRadioButton then
-      (AObject as TRadioButton).Color := AColor
-    else if AObject is TCustomCombo then
-    begin
-      if AClearText then
-        (AObject as TComboBox).Text := '';
-      (AObject as TComboBox).Color := AColor;
+      RttiProperty := RttiType.GetProperty('Color');
+      if Assigned(RttiProperty) then
+        RttiProperty.SetValue(AObject, AColor);
+      RttiProperty := RttiType.GetProperty('Text');
+      if Assigned(RttiProperty) and AClearText then
+        RttiProperty.SetValue(AObject, '');
     end;
   end;
 end;
 
+procedure TValidationResult.SetHint(const AObject: TObject; ABrokenRules: TBrokenRules);
+var
+  Rule, Hint: string;
+begin
+  if Assigned(AObject) then
+  begin
+    if AObject is TControl then
+    begin
+      for Rule in ABrokenRules do
+        Hint := Hint + Rule + #13#10;
+      (AObject as TControl).Hint := Trim(Hint);
+      (AObject as TControl).ShowHint := Hint <> '';
+    end;
+  end;
+end;
+
+procedure TValidationResult.SetValidationColor(AValidationColor: TColor);
+begin
+  FValidationColor := AValidationColor;
+end;
+
 end.
+
